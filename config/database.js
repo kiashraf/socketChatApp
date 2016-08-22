394edd5ecf9a9a5a1cacd16f8babfbd7b82cde52
kiashraf <kiashraf@rediffmail.com>
2016-08-22 07:23:20 +0530

    EVEN SmallER mistakes

--- config/database.js
+++ config/database.js
@@ -9,7 +9,7 @@ if(process.env.NODE_ENV === 'production') {
 
     module.exports = {
 
-        PORT : process.env.host || "",
+        PORT : process.env.PORT || "",
         dbURI: process.env.dbURI,
         sessionSecret: process.env.sessionSecret,
     }
