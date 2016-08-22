d7e9623dd84138ce24859b7d0b8c5cb032fecdc4
kiashraf <kiashraf@rediffmail.com>
2016-08-22 22:18:35 +0530

    host added to socket chat

--- config/database.js
+++ config/database.js
@@ -3,21 +3,21 @@
  */
 'use strict';
 
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     // Offer production stage environment variables
 //  mongodb://kiashraf:kiashraf@ds013486.mlab.com:13486/socketchatapp
 
     module.exports = {
-        host : process.env.host,
-        PORT : process.env.PORT,
+        host: process.env.host || "",
+        PORT: process.env.PORT || "",
         dbURI: process.env.dbURI,
         sessionSecret: process.env.sessionSecret,
     }
 } else {
-    module.exports =  {
+    module.exports = {
 
-        "host" : "http://127.0.0.1:3000",
-         "PORT" : "http://127.0.0.1:3000",
+        "host": "http://127.0.0.1:3000",
+        "PORT": "http://127.0.0.1:3000",
         "dbURI": 'mongodb://127.0.0.1:27017/socketChatApp',
         "sessionSecret": "keyboardkafhlfaflhfdafcat"
 
